1f09e474135ca327a3bd5231d0802ed8ea5e83e1
jvollmar <WVNBCALDWELL@GMAIL.COM>
2025-07-13 16:58:59 -0500

    Update and rename emailer.py.txt to emailer.py

---
+++ emailer.py
@@ -0,0 +1,14 @@
+import smtplib
+from email.message import EmailMessage
+from config import EMAIL_SENDER, EMAIL_PASSWORD, EMAIL_RECEIVER
+
+def send_email(body):
+    msg = EmailMessage()
+    msg["Subject"] = "Weekly Truck Search Results"
+    msg["From"] = EMAIL_SENDER
+    msg["To"] = EMAIL_RECEIVER
+    msg.set_content(body)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
+        smtp.send_message(msg)
