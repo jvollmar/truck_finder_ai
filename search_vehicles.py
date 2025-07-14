9d93b995ddb63f0da99cf03f7ce8b256a845d990
jvollmar <WVNBCALDWELL@GMAIL.COM>
2025-07-14 14:35:58 -0500

    Update search_vehicles.py

--- search_vehicles.py
+++ search_vehicles.py
@@ -1,18 +1,8 @@
 # search_vehicles.py
 from config import VEHICLE_FILTERS
-import requests
-from bs4 import BeautifulSoup
-
-#def search_official_portal(make, model):
-    # Example skeleton for Cadillac portal scraping
-    # You’ll perform HTTP GETs, parse listings, collect image, price, dealer info, etc.
-    listings = []
-    # for each page:
-    # soup = BeautifulSoup(...)
-    # for each result: parse details into dict
-   # return listings
 
 def search_official_portal(make, model):
+    # Mock listing to test HTML output
     return [{
         "title": f"{make} {model} Test Truck",
         "price": "$42,500",
@@ -30,7 +20,7 @@ def search_official_portal(make, model):
 
 def search_vehicles():
     all_listings = []
-    for make in ["GMC", "Chevrolet"]:
+    for make in VEHICLE_FILTERS["make"]:
         for model in VEHICLE_FILTERS["model"]:
             all_listings += search_official_portal(make, model)
     return all_listings
